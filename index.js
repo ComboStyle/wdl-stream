f25f4bf9b05bf6d88564e53678119a75fb1f3ea0
kfir <kfirgez@gmail.com>
2016-11-07 14:08:29 +0200

    implement the wdl parsing

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = wdlstream;
 module.exports.Stream = Stream;
 
 var NEW_LINE = new Buffer( "\n" )[ 0 ]
+var FIELDS_WORD = "#Fields:";
 
 function wdlstream ( options ) {
     return new Stream( options )
@@ -17,6 +18,7 @@ function Stream( options ) {
 
     stream.Transform.call( this, options )
     this.buf = new Buffer( "" );
+    this.headers = null;
 }
 
 Stream.prototype._transform = function( data, enc, done ) {
@@ -24,7 +26,7 @@ Stream.prototype._transform = function( data, enc, done ) {
     // prepend the left-over characters from the previous iteration
     data = Buffer.concat( [ this.buf, data ] );
 
-    // parse the internal querystrings separated by new lines
+    // parse the internal logs separated by new lines
     var start = 0;
     for ( var i = 0 ; i < data.length ; i += 1 ) {
         if ( data[ i ] === NEW_LINE ) {
@@ -56,12 +58,26 @@ Stream.prototype._flush = function ( done ) {
 Stream.prototype._parse = function ( data ) {
     data = data.toString().trim()
 
-    // disregard the question mark prefix
-    if ( data[ 0 ] == "?" ) {
-        data = data.slice( 1 );
-    }
-
-    if ( data.length ) {
-        return data;
+    // it is a command
+    if ( data[ 0 ] == "#" ) {
+        var fieldsIndex = data.indexOf(FIELDS_WORD);
+        // FIELDS command will tell us the headers
+        if (fieldsIndex > -1) {
+            // remove the '#Fields: ' word (including the whitespace hence +1)
+            data = data.slice(fieldsIndex + FIELDS_WORD.length + 1)
+            // set headers(fields)
+            this.headers = data.split(' '); // whitespace separated
+        }
+    // it is a data row
+    } else if ( data.length ) {
+        var result = {};
+        var parts = data.split("\t"); // tab separated
+        for (var i = 0; i < parts.length; i++) {
+            if (parts[i] == '-') {
+                continue; // it means empty value
+            }
+            result[this.headers[i]] = parts[i];
+        }
+        return result;
     }
 }
